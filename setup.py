86b330ad49244475946c5ad5f94b59ab81485737
lnthach <lenguyenthach141@gmail.com>
2023-01-30 03:33:15 +0000

    test pypi files

--- setup.py
+++ setup.py
@@ -7,18 +7,21 @@ from setuptools import Extension, setup
 from Cython.Build import cythonize
 
 setup(
-    name = "mrsqm",
-    version = "0.0.1",
+    #name = "mrsqm",
+    #version = "0.0.1",
     #packages = ["mrsqm"],
     ext_modules = cythonize(Extension(
             name="mrsqm",                                # the extension name
-            sources=["mrsqm_wrapper.pyx","sfa/MFT.cpp","sfa/DFT.cpp","sfa/SFA.cpp","sfa/TimeSeries.cpp"], # the Cython source and
+            sources=["src/mrsqm/mrsqm_wrapper.pyx","src/mrsqm/sfa/MFT.cpp","src/mrsqm/sfa/DFT.cpp","src/mrsqm/sfa/SFA.cpp","src/mrsqm/sfa/TimeSeries.cpp"], # the Cython source and
                                                   # additional C++ source files
             #sources=["mrsqm_wrapper.pyx"],
-            extra_compile_args=["-Wall", "-Ofast", "-g", "-std=c++11",  "-ffast-math"],
+            extra_compile_args=["-Wall", "-Ofast", "-g", "-std=c++11", "-ffast-math"],
             extra_link_args=["-lfftw3", "-lm", "-L/opt/local/lib"],           
             language="c++",                        # generate and compile C++ code
 
-      )))
+      )),
 
-
+    install_requires=["numpy"],
+      
+      
+      )
